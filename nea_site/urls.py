b1dc36c0ed0863acf11a7f7d65267bd33c708ff3
mohan sam <mohanraj.mk1111@gmail.com>
2023-01-10 16:30:32 +0530

    updated media URL

--- nea_site/urls.py
+++ nea_site/urls.py
@@ -35,4 +35,7 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
     path('', include('pages.urls')),
     path('password-change/', ChangePasswordView.as_view(), name='password_change')
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
