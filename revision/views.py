8c9acd0b44eef85d57f50b48a08f19f08eb7ed11
mohan sam <mohanraj.mk1111@gmail.com>
2023-01-17 09:50:57 +0530

    added Linked List logic to list topics end point

--- revision/views.py
+++ revision/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from user_profiles.models import Profile2
 from academic.models import Topics
+from .utils import LinkedList,get_unique_value_from_li
 
 @login_required(login_url=reverse_lazy('login'))
 def revision_resources(request):      
@@ -25,14 +26,17 @@ def revision_resources(request):
 
 
  
+ # used Linked list and Merged sort
 @login_required(login_url=reverse_lazy('login'))
 def list_topics(request,subject_id):     
-    topics=[]   
+    li = LinkedList()
     topics_by_subject=Topics.objects.filter(subject_id=subject_id)
     for topic in topics_by_subject:
-         topics.append(topic.topics_name)    
-    topics=list(set(topics))
-    return render(request, 'revision/list_topics.html', {'topics':topics})
+        li.append(topic.topics_name)
+    # there can be duplicate topics, to get unique value from Li, first doing merge sort and then getting unique value from Li
+    li.head=li.mergeSort(li.head)  
+    value=get_unique_value_from_li(li.head)
+    return render(request, 'revision/list_topics.html', {'topics':value})
 
 
 @login_required(login_url=reverse_lazy('login'))
