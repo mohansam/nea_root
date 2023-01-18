e9487c6ede3ee2a487918670b79502c19bd8dc8c
mohan sam <mohanraj.mk1111@gmail.com>
2023-01-18 22:13:17 +0530

    added priorty to cal

--- cal/models.py
+++ cal/models.py
@@ -4,8 +4,13 @@ from django.contrib.auth.models import User
 
 from django.urls import reverse
 
+EVENT_PRIORITY=( ('HIGH', 'HIGH'),
+    ('Medium', 'Medium'),
+    ('Low', 'Low'))
+
 class Event(models.Model):
     title = models.CharField(max_length=100)
+    priority=models.CharField(max_length=20, choices=EVENT_PRIORITY)
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
@@ -15,4 +20,5 @@ class Event(models.Model):
     @property
     def get_html_url(self):
         url = reverse('cal:event_edit', args=(self.id,))
-        return f'<a href="{url}"> {self.title} </a>'
+        event_type_class={'HIGH':'eventP1','Medium':'eventP2','Low':'eventP3'}
+        return f'<a  class="{event_type_class[self.priority]}" href="{url}"> <b>{self.title}</b> </a>'
