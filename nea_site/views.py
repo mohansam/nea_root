9ea7875e0cf93568969ad5d6e59539fcb39a0033
mohan sam <mohanraj.mk1111@gmail.com>
2023-02-18 13:10:17 +0530

    updated home page

--- nea_site/views.py
+++ nea_site/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+@login_required(login_url=reverse_lazy('login'))
 def homepage(request):
-    
     return render(request, 'home.html')
