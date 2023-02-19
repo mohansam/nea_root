678ed7521be36b3dc1087bc2350c31d27164fcd7
mohan sam <mohanraj.mk1111@gmail.com>
2023-02-19 14:19:41 +0530

    updated home page

--- nea_site/views.py
+++ nea_site/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta, date
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from cal.models import Event
+
 # Create your views here.
 
 @login_required(login_url=reverse_lazy('login'))
 def homepage(request):
-    return render(request, 'home.html')
+    today_date=datetime.now()
+    user_id=request.user.id
+    current_month_events = Event.objects.filter(start_time__year=today_date.year, start_time__month=today_date.month , username_id=user_id)
+    todays_event_day = current_month_events.filter(Q(start_time__day=today_date.day)|Q(end_time__day=today_date.day))
+    return render(request, 'home.html',{'events':todays_event_day})
